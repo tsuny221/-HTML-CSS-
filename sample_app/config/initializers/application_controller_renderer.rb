5ee5e0166e39311714c4ffb6e2cc842bf7620f4e
tsuny221 <kingoftsuny@gmail.com>
2020-05-02 15:17:49 +0900

    [Update] 3-10

---
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '68cb2eb058a48402bec6dcd574dd6459e4e34d85a905f66b7182fc112eb6ec6c6a305020e0a134dedeb670bc95ef1649fa7884bef685050d4a85209aab70a3ca'
