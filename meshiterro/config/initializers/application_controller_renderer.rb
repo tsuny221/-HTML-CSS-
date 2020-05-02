015203e4e330afc7d76e08a9510b436d77f0a47d
常松美彩 <tsuny@tsunematsubisainoMacBook-Pro.local>
2020-05-02 15:24:40 +0900

    [Finish] 5-7

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+Refile.secret_key = '888a0f710bf73438a05fb914c43da248d762a47410da33c3c7b89a62b93684a14f76a0284e1408ed23b6d69bedad372cf9ff0b3f8cd98e1a4173ced9f891ccb5'
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
