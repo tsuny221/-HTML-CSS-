78692c484c39126d3790d36acda0e36bef82f5ee
常松美彩 <tsuny@tsunematsubisainoMacBook-Pro.local>
2020-05-02 15:19:12 +0900

    [Update] 3-10(review)

---
+++ sample2_app/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,11 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '711bcb799eeed48a6409459c22b432f42774eedb29274be0575d190c63ad2a5bad4a1a1a0f3488844341ce45e2ab767ab25b0c992cb954ea2655c8e5d65e3b22'
+
