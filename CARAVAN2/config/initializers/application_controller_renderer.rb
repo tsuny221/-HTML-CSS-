045e986da010b791a642a6c91339773bd40ec7bc
tsuny221 <kingoftsuny@gmail.com>
2020-05-02 15:12:47 +0900

    [Finish] 4-7(review)

---
+++ CARAVAN2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'c03ed6b8c0bffb9f7de5a73f257a381d93e5723356999a2de86d24d39cc84204ca3dd1ca05739cea36650c5e2c8bd7182332b55d0f4653cbd3a54bbace42ecbd'
