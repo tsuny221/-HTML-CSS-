0e4ea323700b7ea7c2f0598161277ff303a98967
常松美彩 <tsuny@tsunematsubisainoMacBook-Pro.local>
2020-05-03 15:10:12 +0900

    [Finish] 5-7(review)

---
+++ meshiterro2/app/assets/config/manifest.js
@@ -0,0 +1,4 @@
+//= link_tree ../images
+//= link_directory ../javascripts .js
+//= link_directory ../stylesheets .css
+Refile.secret_key = '44c788b9f965ea7b136a58aaab52682562d90bfee3ab7fd4b08d658ef5c9ccdb3e08a762038589d878426ce0dfabfdb9a5520306198381b135b8e72b3a615d97'
---
+++ meshiterro2/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '44c788b9f965ea7b136a58aaab52682562d90bfee3ab7fd4b08d658ef5c9ccdb3e08a762038589d878426ce0dfabfdb9a5520306198381b135b8e72b3a615d97'
