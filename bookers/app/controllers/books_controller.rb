efbd7ca4f95088529eb5f95b52453b4d5dcfb008
常松美彩 <tsuny@tsunematsubisainoMacBook-Pro.local>
2020-05-04 19:04:00 +0900

    [Start] 5-9

---
+++ bookers/app/controllers/books_controller.rb
@@ -0,0 +1,43 @@
+class BooksController < ApplicationController
+   def index
+  	@books = Book.all
+  	@book = Book.new
+  end
+  def create
+  	@book = Book.new(book_params)
+  	if @book.save
+  		flash[:notice] = "You have creatad book successfully."
+  		redirect_to books_path(@book.id)
+  	else
+      @books = Book.all
+  		render :index
+  	end
+  end
+   def show
+  	@book = Book.find(params[:id])
+  	@book = Book.new
+  end
+
+  def edit
+  	@book = Book.find(params[:id])
+  end
+
+  def update
+  	@book = Book.find(params[:id])
+  	if @book.update(book_params)
+  		flash[:notice] = "You have creatad book successfully."
+  		redirect_to book_path(@book)
+  	else
+  		render :edit
+  	end
+  end
+  def destroy
+  	book = Book.find(params[:id])
+  	book.destroy
+  	redirect_to books_path
+  end
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+end
