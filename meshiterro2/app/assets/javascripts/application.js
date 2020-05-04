b96c14f959c03082787fd96ddd3e4859dac3b768
常松美彩 <tsuny@tsunematsubisainoMacBook-Pro.local>
2020-05-04 19:01:01 +0900

    [Finish] 6-13

--- meshiterro2/app/assets/javascripts/application.js
+++ meshiterro2/app/assets/javascripts/application.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
       // スライドショーの変化 ("fade" or "slide")
       transition : 'slide',
       // 変化に係る時間(ミリ秒)
-      speed : 1000,
+      speed : 2000,
       // easingの種類
       easing : 'easeOutQuart',
       // ナビゲーションの形("block" or "bubble")
@@ -15,13 +15,14 @@ $(document).ready(function () {
       // スライドショーの自動再生(falseで自動再生なし)
       autoPlay : true,
       // 自動再生時のスライド切替間隔(ミリ秒)
-      autoPlayDuration : 3000,
+      autoPlayDuration : 5000,
       // キーボードの矢印キーによるスライド送りの設定(trueで有効)
       keyboardOnAlways : true,
       // 一枚目のスライド表示時に戻る矢印を表示するかどうか(falseで非表示)
       hidePrevious : false
   });
 });
+
 // This is a manifest file that'll be compiled into application.js, which will include all the files
 // listed below.
 //
@@ -35,7 +36,6 @@ $(document).ready(function () {
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require rails-ujs
-//= require turbolinks
+//= require activestorage
 //= require_tree .
