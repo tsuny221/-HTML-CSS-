73559ee89cec35ed67261d9cbae3c8564a92a34a
tsuny221 <kingoftsuny@gmail.com>
2020-05-05 13:47:31 +0900

    [Update] 5-9

--- bookers/config/routes.rb
+++ bookers/config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 root to: 'homes#top'
 get 'home/about' => 'homes#about'
-resources :books
 devise_for :users
+resources :books 
+root 'books#index'
+resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
