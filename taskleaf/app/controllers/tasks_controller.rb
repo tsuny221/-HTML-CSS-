73feec829c82eca5b0182b1f11d03208fd159fad
tsuny221 <kingoftsuny@gmail.com>
2020-05-16 19:21:41 +0900

    [Update] railsguide 4-10

---
+++ taskleaf/app/controllers/tasks_controller.rb
@@ -0,0 +1,46 @@
+class TasksController < ApplicationController
+   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  def index
+    @tasks = Task.where(user_id: current_user.id)
+  end
+
+  def show
+    @task = current_user.tasks.find(params[:id])
+  end
+
+  def new
+  	@task = Task.new
+  end
+  def create
+  	@task = current_user.tasks.new(task_params)
+  	if @task.save
+  	redirect_to tasks_path, notice: "タスク「#{task.name}」を登録しました。"
+  else
+    render :new
+  end
+  end
+
+  def edit
+    @task = current_user.tasks.find(params[:id])
+  end
+  def update
+    @task = current_user.tasks.find(params[:id])
+   if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスク「#{@task.name}」を更新しました。"
+    else
+      render :edit
+  end
+  end
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy
+    redirect_to tasks_path, notice: "タスク「#{task.name}」を削除しました。"
+  end
+  private
+  def task_params
+  	params.require(:task).permit(:name, :description)
+  end
+  def set_task
+    @task = current_user.tasks.find(params[:id])
+  end
+end
